ea3a77abd92383c8085c649fe622a8226490a4fc
yogimardilah <yogimardilah19@gmail.com>
2021-03-19 15:03:11 +0700

    update db

--- index.php
+++ index.php
@@ -14,3 +14,20 @@ if(mysqli_connect_errno()){
 }else{
 	echo 'Koneksi berhasil ^_^';
 }
+
+
+
+
+$host		="localhost";
+$user		="yogi";
+$pass		="Dbdigitalocean2021!";
+$db 		="project_gis_pasarcisaat";
+
+$koneksi2=mysql_connect($host,$user,$pass);
+mysql_select_db($db,$koneksi);	
+
+if(mysql_connect_errno()){
+	echo 'Gagal melakukan koneksi ke Database : '.mysql_connect_error();
+}else{
+	echo 'Koneksi berhasil ^_^';
+}
