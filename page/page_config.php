3a346db9aee5fb9961d346b400d40e5ef29594ba
yogimardilah <yogimardilah19@gmail.com>
2021-03-19 15:39:09 +0700

    update db

--- page/page_config.php
+++ page/page_config.php
@@ -11,8 +11,9 @@
 	$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 	 
 	if(mysqli_connect_errno()){
-		echo 'Gagal melakukan koneksi ke Database : '.mysqlii_connect_error();
+		echo 'Gagal melakukan koneksi ke Database : '.mysqli_connect_error();
 	}else{
 		echo 'Koneksi berhasil ^_^';
 	}
+	echo 'tes ^_^';
 ?>
