34533f7537d23ffaaea0c586b60426134a9af291
yogimardilah <yogimardilah19@gmail.com>
2021-03-19 15:06:18 +0700

    update db

--- index.php
+++ index.php
@@ -16,18 +16,20 @@ if(mysqli_connect_errno()){
 }
 
 
-
-
-$host		="localhost";
-$user		="yogi";
-$pass		="Dbdigitalocean2021!";
-$db 		="project_gis_pasarcisaat";
-
-$koneksi2=mysql_connect($host,$user,$pass);
-mysql_select_db($db,$koneksi);	
-
-if($koneksi2){
-	echo 'Gagal melakukan koneksi ke Database : ';
-}else{
-	echo 'Koneksi berhasil ^_^';
+$nama_db = 'project_gis_pasarcisaat';
+$user_db = 'yogi';
+$kata_sandi = 'Dbdigitalocean2021!';
+$nama_host = 'localhost';
+$koneksi = mysqli_connect($nama_host, $user_db, $kata_sandi) or die("Tidak bisa tersambung ke '$nama_host'");
+mysqli_select_db($koneksi, $nama_db) or die("Tidak bisa membuka database '$nama_db'");
+$uji_query = "SHOW TABLES FROM $nama_db";
+$hasil = mysqli_query($link, $uji_query);
+$hitung_tabel = 0;
+while($tabel = mysqli_fetch_array($hasil)) {
+  $hitung_tabel++;
+}
+if (!$hitung_tabel) {
+  echo "Tidak ada tabel<br />\n";
+} else {
+  echo "Ada $hitung_tabel tabel<br />\n";
 }
