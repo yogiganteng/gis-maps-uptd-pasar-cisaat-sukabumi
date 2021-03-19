98dc6ce046d3f864c1d4b385997dfc530e270fbb
yogimardilah <yogimardilah19@gmail.com>
2021-03-19 15:32:30 +0700

    update db

--- admin/page/page_config.php
+++ admin/page/page_config.php
@@ -1,11 +1,17 @@
 <?php
 	ini_set('display_errors',FALSE);
-
-	$host		="localhost";
-	$user		="yogi";
-	$pass		="Dbdigitalocean2021!";
-	$db 		="project_gis_pasarcisaat";
-
-	$koneksi=mysql_connect($host,$user,$pass);
-	mysql_select_db($db,$koneksi);	
+	$db_host = "localhost";
+	$db_user = "yogi";
+	$db_pass ="Dbdigitalocean2021!";
+	$db_name 	="project_gis_pasarcisaat";
+	
+	
+	 
+	$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+	 
+	if(mysqli_connect_errno()){
+		echo 'Gagal melakukan koneksi ke Database : '.mysqli_connect_error();
+	}else{
+		echo 'Koneksi berhasil ^_^';
+	}
 ?>
